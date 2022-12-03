9687be93d4b7ef6641995bb09e36484c3f9b8ec0
Chandler <chandlerzhao1142@gmail.com.cn>
2022-12-03 21:39:20 +0800

    DESC: add ribbon

--- nacos-study/service/employee-service/employee-api/src/main/java/com/imooc/employee/api/IEmployeeActivityService.java
+++ nacos-study/service/employee-service/employee-api/src/main/java/com/imooc/employee/api/IEmployeeActivityService.java
@@ -2,10 +2,14 @@ package com.imooc.employee.api;
 
 import com.imooc.employee.pojo.EmployeeActivity;
 
+import java.util.List;
+
 public interface IEmployeeActivityService {
 
     EmployeeActivity useToilet(Long employeeId);
 
     EmployeeActivity restoreToilet(Long employeeId);
+
+    List<Long> getAvailableIds();
 
 }
--- nacos-study/service/employee-service/employee-core/src/main/java/com/imooc/employee/service/EmployeeService.java
+++ nacos-study/service/employee-service/employee-core/src/main/java/com/imooc/employee/service/EmployeeService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -94,6 +96,14 @@ public class EmployeeService implements IEmployeeActivityService {
         BeanUtils.copyProperties(record, result);
 
         return result;
+    }
+
+    @GetMapping("getAvailableIds")
+    public List<Long> getAvailableIds() {
+        List<Toilet> availableToilet = restroomService.getAvailableToilet();
+        return availableToilet.stream().map(element -> {
+            return element.getId();
+        }).collect(Collectors.toList());
     }
 
 }
